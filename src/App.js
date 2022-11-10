2467b5b6d6bd260cdd6fce98bd578c49c24d65ef
Deepak Kumar Fathkar <deepak.fathkar@gmail.com>
2022-11-10 11:07:19 +0530

    chakra installer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import AllRoutes from "./Router/AllRoutes"
 import Navbar from "./Components/Navbar";
+import AboveNav from "./Components/AboveNav";
 
 function App() {
   return <>
+  <AboveNav />
   <Navbar />
   <AllRoutes />
   </>
---
+++ src/Components/AboveNav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Flex, Spacer,Box } from '@chakra-ui/react'
+
+const AboveNav = () => {
+  return (
+    <div>
+        <Flex>
+  <Box p='4' bg='red.400'>
+    Box 1
+  </Box>
+  <Spacer />
+  <Box p='4' bg='green.400'>
+    Box 2
+  </Box>
+</Flex>
+    </div>
+  )
+}
+
+export default AboveNav
