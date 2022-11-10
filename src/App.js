e6205e5da9034b8d2995cb9f3d57e4a573025d5a
Deepak Kumar Fathkar <deepak.fathkar@gmail.com>
2022-11-10 13:44:16 +0530

    below navbar done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import AllRoutes from "./Router/AllRoutes"
 import Navbar from "./Components/Navbar";
 import AboveNavbar from "./Components/AboveNavbar";
+import BelowNav from "./Components/BelowNav";
 
 function App() {
   return <>
   <AboveNavbar />
   <Navbar />
+  <BelowNav />
   <AllRoutes />
   </>
 }
---
+++ src/Components/BelowNav.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGift } from "@fortawesome/free-solid-svg-icons";
+import { Stack, Button, Text } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
+
+const BelowNav = () => {
+  return (
+    <Center mt="10px" color="black" fontSize="20px">
+      <Stack spacing="25px" direction="row">
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            <FontAwesomeIcon icon={faGift} /> Holiday Gifts
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            Holiday Deals
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            Women
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            Men
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            Kids
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            Young Adult
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            Activewerw
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            Home
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            Beauty
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            Designer
+          </Text>
+        </Button>
+        <Button colorScheme="white">
+          <Text
+            _hover={{
+              color: "teal.500",
+              textDecoration: "underline",
+            }}
+          >
+            The Thread
+          </Text>
+        </Button>
+      </Stack>
+    </Center>
+  );
+};
+
+export default BelowNav;
