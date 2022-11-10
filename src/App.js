2576774b546e60034f5d14cd692e1875ab497a1f
Deepak Kumar Fathkar <deepak.fathkar@gmail.com>
2022-11-10 12:05:05 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 
 import AllRoutes from "./Router/AllRoutes"
 import Navbar from "./Components/Navbar";
-import AboveNav from "./Components/AboveNav";
 
 function App() {
   return <>
-  <AboveNav />
   <Navbar />
   <AllRoutes />
   </>
